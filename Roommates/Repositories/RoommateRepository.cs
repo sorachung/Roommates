91cb7f713d0ecf107b84c9195ebf7eb45afe1e97
sorachung <sorachung.src@gmail.com>
2022-03-17 11:06:09 -0500

    implemented GetAll method in RoommateRepository

--- Roommates/Repositories/RoommateRepository.cs
+++ Roommates/Repositories/RoommateRepository.cs
@@ -22,7 +22,7 @@ namespace Roommates.Repositories
                     cmd.CommandText = "SELECT * FROM Roommate JOIN Room ON Room.Id = Roommate.RoomId WHERE Roommate.Id = @id";
                     cmd.Parameters.AddWithValue("@id", id);
 
-                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
                         Roommate roommate = null;
                         Room room = null;
@@ -46,6 +46,49 @@ namespace Roommates.Repositories
                             };
                         }
                         return roommate;
+                    }
+                }
+            }
+        }
+
+        public List<Roommate> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT * FROM Roommate
+                        JOIN Room ON Room.Id = Roommate.RoomId";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Roommate> roommates = new List<Roommate>();
+
+                        while (reader.Read())
+                        {
+
+                            Room room = new Room()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("RoomId")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                MaxOccupancy = reader.GetInt32(reader.GetOrdinal("MaxOccupancy"))
+                            };
+
+                            Roommate roommate = new Roommate()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                MovedInDate = reader.GetDateTime(reader.GetOrdinal("MoveInDate")),
+                                Room = room
+                            };
+
+                            roommates.Add(roommate);
+                        }
+                        return roommates;
                     }
                 }
             }
